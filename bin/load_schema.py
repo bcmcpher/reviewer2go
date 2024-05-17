9d23b613922e53d78743f8f677f062c49c7dfe28
Brent McPherson <bcmcpher@gmail.com>
2024-05-17 13:59:29 -0400

    added initial parsing from schema

---
+++ bin/load_schema.py
@@ -0,0 +1,62 @@
+"""
+Load the Reproschema to build LLM prompts.
+"""
+
+import json
+from pathlib import Path
+import re
+
+# look for protocols
+protocol = "cobidas"
+
+# path to files
+root = "/home/bcmcpher/Projects/brainhack2023/reviewer2go/cobidas_schema"
+
+# paths to the folders
+label_folder = Path(root, "labels", protocol)
+schema_folder = Path(root, "schemas")
+protocol_file = Path(schema_folder, protocol, "protocols", f"{protocol}_schema.jsonld")
+
+# for some reason, the pythonic relative paths wouldn't resolve?
+
+# load the protocol file
+with open(protocol_file) as f:
+    protocol_json = json.load(f)
+
+# extract the list of individual protocols that are part of this schema
+activities_order = protocol_json["ui"]["order"]
+
+# for every individual schema
+for i, activity in enumerate(activities_order):
+
+    # extract the activity features
+    activity_file = protocol_file.parent / activity
+    activity_name = activity_file.stem.replace("_schema", "")
+    print(f"Processing activity #{i:>01}: {activity_name}")
+
+    # load the activity schema
+    with open(activity_file) as f:
+        activity_json = json.load(f)
+
+    # pull the activity elements
+    items_order = activity_json["ui"]["order"]
+
+    # for every question in the activity
+    for j, item in enumerate(items_order):
+
+        # load the questions schema
+        with open(activity_file.parent / item) as f:
+            item_json = json.load(f)
+
+        # pull the english question stem
+        question = item_json["question"]["en"]
+
+        # remove anything after <div>
+        if "<div" in question:
+            question = question.split("<div")[0]
+
+        # clean the question stem
+        cquest = re.sub(".* - ", "", question)
+
+        # print with new structures to track
+        print(f" -- {i:>02}.{j:>02} - {cquest}")
